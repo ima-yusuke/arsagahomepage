21e24488db359c21b4f962a268a1a9cd127dbff0
ima-yusuke <yusuke1125aadxyz@gmail.com>
2023-08-21 14:32:11 -0700

    home forth section done

--- src/component/homeForthSection/homeForthSection.js
+++ src/component/homeForthSection/homeForthSection.js
@@ -1,12 +1,12 @@
 import styles from "./homeForthSection.module.scss";
+import homeForthImg from "./homeForthSection.jpg";
+import "font-awesome/css/font-awesome.min.css";
 
 function HomeForthSection() {
   return (
     <>
-      <section>
-        <section>
-          <h2>OUR EXPERTISE</h2>
-        </section>
+      <section className={styles.parentSection}>
+        <h2 className={styles.backH2}>OUR EXPERTISE</h2>
         <section className={styles.section}>
           <article>
             <h2>
@@ -14,8 +14,61 @@ function HomeForthSection() {
               <span>ソリューション</span>
             </h2>
             <p>
-            DXを活用した幅広い戦略コンサルティングとIT開発により、お客様のビジネスを成功へと導きます
+              DXを活用した幅広い戦略コンサルティングとIT開発により、お客様のビジネスを成功へと導きます
             </p>
+          </article>
+          <article className={styles.imgAndIcons}>
+            <img src={homeForthImg}></img>
+            <aside>
+              <aside>
+                <div>
+                  <i className="fa fa-solid fa-calculator"></i>
+                  <p>
+                    DXコンサル<br></br>ティング
+                  </p>
+                </div>
+                <div>
+                  <i className="fa fa-solid fa-clipboard"></i>
+                  <p>
+                    IT・<br></br>システム開発
+                  </p>
+                </div>
+                <div>
+                  <i className="fa fa-solid fa-folder"></i>
+                  <p>ウェブ制作</p>
+                </div>
+                <div>
+                  <i className="fa fa-solid fa-globe"></i>
+                  <p>アプリ開発</p>
+                </div>
+                <div>
+                  <i className="fa fa-solid fa-calculator"></i>
+                  <p>
+                    UX/UI戦略・<br></br>デザイン
+                  </p>
+                </div>
+                <div>
+                  <i className="fa fa-solid fa-clipboard"></i>
+                  <p>QA・テスト</p>
+                </div>
+                <div>
+                  <i className="fa fa-solid fa-folder"></i>
+                  <p>
+                    Web<br></br>マーケティング
+                  </p>
+                </div>
+                <div>
+                  <i className="fa fa-solid fa-globe"></i>
+                  <p>
+                    カスタマー<br></br>サービス
+                  </p>
+                </div>
+              </aside>
+              <div className={styles.button4}>
+                <div className={styles.eff4}></div>
+                <a href="#">詳しく見る →</a>
+              </div>
+            </aside>
           </article>
         </section>
       </section>
